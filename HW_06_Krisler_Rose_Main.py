873f2342fbcc40ae58d8e3ec236cb9284d48c65c
Mae Krisler <mae@bkds.com>
2025-10-29 12:49:42 -0400

    csv parsing added

--- HW_06_Krisler_Rose_Main.py
+++ HW_06_Krisler_Rose_Main.py
@@ -1,1 +1,11 @@
 # main function for agglomeration
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    filename = "HW_CLUSTERING_SHOPPING_CART_v2245a.csv"
+    df = pd.read_csv(filename)
+
+    print(df.head())
